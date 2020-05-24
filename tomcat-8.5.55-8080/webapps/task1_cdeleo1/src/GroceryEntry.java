12b0c8104ebcd9cd4ed970e11e11681436e5a9b6
Cristi DeLeo <cdeleo1@asu.edu>
2020-05-24 02:02:49 -0400

    [05/24/2020] Lab1: Task1 successful deployment to Landing Page

---
+++ tomcat-8.5.55-8080/webapps/task1_cdeleo1/src/GroceryEntry.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.*;
+
+public class GroceryEntry {
+    private String itemname;
+    private String quantity;
+    private String category;
+    private String brand;
+    private String aisle;
+
+    public GroceryEntry(String itemname, String quantity, String category,
+                String brand, String aisle)
+    {
+	this.itemname  = itemname;
+	this.quantity  = quantity;
+	this.category = category;
+        this.brand = brand;
+        this.aisle = aisle;
+    }
+
+    public void changeQuantity(String newquantity) {
+    	quantity = newquantity;
+    	// This is here to introduce artifical latency for testing purposes
+    	try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public String toString()
+    { return itemname + "\n" + quantity + "\n" + category + "\n" + brand
+            + "\n" + aisle; }
+}
+
+
+
